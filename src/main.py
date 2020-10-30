04da194a8a7e2c74e9623ceb9339763c639a6717
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-10-30 22:04:53 +0330

    add some comments

--- src/main.py
+++ src/main.py
@@ -2,10 +2,16 @@ import lexer
 
 def main():
     
+    # Read the current flow source code in input.lang and store it in variable
     content = ""
     with open("../tests/input.lang" , 'r') as file:
         content = file.read()
     
+    #
+    # Lexer
+    #
+        
+    # Call the lexer class and initilize it with source code
     main_lexer = lexer.Lexer(content)
     main_lexer.lex()
      
